c9f2181e6d7cd577cf13b178a44364f38f7221d9
Yesudeep Mangalapilly <yesudeep@gmail.com>
2010-08-05 15:25:19 +0530

    Automatic configuration for erc

--- config-erc.el
+++ config-erc.el
@@ -1,3 +1,11 @@
 
+(setq erc-autojoin-channels-alist
+      '(("freenode.net" "#emacs" "##linux-india")
+        ("irc.mozilla.org" "#firefox")))
+
+;; Servers you want to connect to.
+(erc :server "irc.freenode.net" :port 6667 :nick "yournick")
+;;(erc :server "irc.mozilla.org" :port 6667 :nick "yournick")
 
 (provide 'config-erc)
+
