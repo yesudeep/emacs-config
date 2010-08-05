7e67680e28286b8a46f4f04330d689240af6ec91
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2010-08-05 12:42:48 +0530

    A little cleanup

--- config-coffeescript.el
+++ config-coffeescript.el
@@ -13,17 +13,14 @@
   (setq coffee-debug-mode t)
 
   ;; Emacs key binding
-  (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
+  (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer))
 
-  ;; Riding edge.
-  (setq coffee-command "~/dev/coffee")
+;; Compile to JS on every save.
+(add-hook 'after-save-hook
+          '(lambda ()
+             (when (string-match "\\.coffee$" buffer-file-name)
+               (coffee-compile-file))))
 
-  ;; Compile to JS on every save.
-  (add-hook 'after-save-hook
-            '(lambda ()
-               (when (string-match "\\.coffee$" buffer-file-name)
-                 (coffee-compile-file))))
-  )
 
 (add-hook 'coffee-mode-hook '(lambda () (coffee-custom)))
 
